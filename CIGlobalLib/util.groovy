bef8586de33f4e18e288aed800c28880475f776b
ohad182 <ohad182@gmail.com>
2016-12-28 15:25:32 +0200

    Update util.groovy

--- CIGlobalLib/util.groovy
+++ CIGlobalLib/util.groovy
@@ -1,22 +1,6 @@
-/*import groovyx.net.http.RESTClient*/
-
 def call(name){
     echo '''
-    _______    _______     ______      ______  ___      ___  ___  ___  
- /" _   "|  /"      \   /    " \    /    " \|"  \    /"  ||"  \/"  | 
-(: ( \___) |:        | // ____  \  // ____  \\   \  //  /  \   \  /  
- \/ \      |_____/   )/  /    ) :)/  /    ) :)\\  \/. ./    \\  \/   
- //  \ ___  //      /(: (____/ //(: (____/ //  \.    //     /   /    
-(:   _(  _||:  __   \ \        /  \        /    \\   /     /   /     
- \_______) |__|  \___) \"_____/    \"_____/      \__/     |___/      
-                                                                    
+called util.groovy from ${name}
 '''
-  /*
-  def client = new RESTClient( 'http://artii.herokuapp.com/' )
-  def resp = client.get( path : 'make?text=${name}&font=rockbox_' ) // ACME boomerang
-
-  assert resp.status == 200  // HTTP response code; 404 means not found, etc.
-  echo resp.getData()  
-     */
   }
 return this
